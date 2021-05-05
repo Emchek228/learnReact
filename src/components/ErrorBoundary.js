750e77b6f6422c582689c826f72db588f6fe5bf3
Emil <emilch9@gmail.com>
2021-05-05 13:51:48 -0700

    change again error boundary

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -8,9 +8,9 @@ class ErrorBoundary extends Component {
         }
     }
 
-    componentDidCatch(error, info) {
-        this.setState({ hasError: true })
-    }
+//    componentDidCatch(error, info) {
+//       this.setState({ hasError: true })
+//    }
 
     render() {
         if (this.state.hasError) {
